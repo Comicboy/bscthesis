6da8515bda61e0972bfe72a9c7803bead1d452af
Comicboy <58115015+Comicboy@users.noreply.github.com>
2020-11-25 23:21:06 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,27 +1,47 @@
 import time
-import traitlets
-from traitlets.config.configurable import SingletonConfigurable
-from .motor import Motor
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-
-class Robot(SingletonConfigurable):
+class Robot():
     
-    left_motor = traitlets.Instance(Motor)
-    right_motor = traitlets.Instance(Motor)
-
-    # config
-    left_motor_channel = traitlets.Integer(default_value=1).tag(config=True)
-    left_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
-    right_motor_channel = traitlets.Integer(default_value=2).tag(config=True)
-    right_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
+    # Pin configurations
+    # Left motor enable, controll pins and PWM for the motor driver's respective pins
+    ENA = 23
+    IN1 = 26
+    IN2 = 29
+    PWMA = 33
+    # Left motor enable, controll pins and PWM for the motor driver's respective pins
+    ENB = 35
+    IN3 = 37
+    IN4 = 38
+    PWMB = 32
+    
     
     def __init__(self, *args, **kwargs):
         super(Robot, self).__init__(*args, **kwargs)
-        self.left_motor = Motor(self.motor_driver, channel=self.left_motor_channel, alpha=self.left_motor_alpha)
-        self.right_motor = Motor(self.motor_driver, channel=self.right_motor_channel, alpha=self.right_motor_alpha)
         
-    def set_motors(self, left_speed, right_speed):
-        self.left_motor.value = left_speed
+        # Setting up the left motor
+        GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(PWMA, GPIO.OUT)
+        self.left_speed = 0 # Variable for controlling the left motor speed
+        
+        # Setting up the right motor
+        GPIO.setup(ENB, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(PWMB, GPIO.OUT)
+        self.right_speed = 0 # Variable for controlling the right motor speed
+        
+        # Setting up the PWM
+        self.pwm = [GPIO.PWM(PWMB,50), GPIO.PWM(PWMA,50)]
+        self.pwm[0].start(0)
+        self.pwm[1].start(1)
+        
+    def set_motors(self, left_speed=1.0, right_speed=1.0):
+        GPIO.output(self.lef
         self.right_motor.value = right_speed
         
     def forward(self, speed=1.0, duration=None):
