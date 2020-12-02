c6ec97b3052dc2bf92adcab17237501a9a7d7fc2
Comicboy <58115015+Comicboy@users.noreply.github.com>
2020-12-02 21:40:08 +0100

    Update collisionavoid.py

--- collisionavoid.py
+++ collisionavoid.py
@@ -10,17 +10,28 @@ from jetcam.csi_camera import CSICamera
 import time
 
 # Load in the model
-model = models.load_model('jetson_inceptionv3.pb')
+model = models.load_model('jetson_inceptionv3.h5')
 
 # Create camera with compatible output dimensions (299,299)
 camera = CSICamera(width=299, height=299, capture_width=1080, capture_height=720, capture_fps=30)
+
+# initializing the motors
+robot = Robot()
 
 def avoid():
   image = camera.read() # reads in the current frame into a (299,299,3) numpy array
   image = preprocess_input(image)
   
-  yhat = model.predict(image)
-  label = decode_predictions(yhat) # Decoding the made prediction and selecting the highest probability
-  label = label[0][0]
+  yhat = model.predict(image) # If the way is free then yhat ~ 1, if it is blocked yhat ~ 0
   
-  prediction = label[2] # The probability of 
+  # If the road is free then go forward, it it is blocked turn left
+  if yhat > 0.5:
+    robot.forward(0.7)
+  else:
+    robot.left(0.7)
+    
+  time.sleep(0.001)
+
+# Calling the avoid function in an infinite loop
+while(True):
+  avoid()
